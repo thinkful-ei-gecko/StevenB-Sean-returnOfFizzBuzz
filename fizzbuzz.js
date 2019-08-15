d01916c3357e8bff0dcdd68ecea86fa9928bacde
Sean Cooper <seankamran@gmail.com>
2019-08-15 15:57:44 -0400

    First Commit

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,17 +2,39 @@
 
 //@param {number} num
 //@returns {*} will return 'fizzbuzz'/'fizz'/'buzz' OR the number itself
-function fizzbuzz(num) {}
+function fizzbuzz(num) {
+    if (num % 15 === 0) {
+        return ('fizzbuzz');
+    }
+    if (num % 5 === 0) {
+        return ('buzz');
+    }
+    if (numb % 3 === 0) {
+        return ('fizz'); 
+    }
+}  
+
 
 /* 
 @param {*} fizzResult will return 'fizzbuzz'/'fizz'/'buzz' OR the number itself
 @returns {string} generate ONE fizzBuzz item 
 */
-function generateFizzHtml(fizzResult) {}
+function generateFizzHtml(fizzResult) {
+    return (`<div class="fizz-buzz-item ${fizzResult}">
+            <span> ${fizzResult} </span>
+            </div>`);
+}
 
 
 //Handles form submission
-function handleSubmit() {}
+function handleSubmit() {
+    const countTo = $('#number-chooser').val('submit', function(event) {
+        event.preventDefault();
+    });
+    .submit 
+
+
+}
 
 
 $(handleSubmit);
