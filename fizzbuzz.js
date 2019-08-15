de89cddba545d715bbfa840c7779698d22a679f4
Steven Bull <stevenwbulljr@gmail.com>
2019-08-15 12:46:57 -0500

    first commit

---
+++ fizzbuzz.js
@@ -0,0 +1,18 @@
+'use strict';
+
+//@param {number} num
+//@returns {*} will return 'fizzbuzz'/'fizz'/'buzz' OR the number itself
+function fizzbuzz(num) {}
+
+/* 
+@param {*} fizzResult will return 'fizzbuzz'/'fizz'/'buzz' OR the number itself
+@returns {string} generate ONE fizzBuzz item 
+*/
+function generateFizzHtml(fizzResult) {}
+
+
+//Handles form submission
+function handleSubmit() {}
+
+
+$(handleSubmit);
